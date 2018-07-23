9f335166063f3b4ce531e60ed3a784506c08ef08
Carlovan <giulio.carlassare@gmail.com>
2018-07-23 17:28:21 +0200

    Ampliata struttura per la query per filtri.

--- lib1718.h
+++ lib1718.h
@@ -10,6 +10,20 @@
 #define ACTION_SELECT 3
 typedef int action_t;
 
+#define FILTER_WHERE 10
+#define FILTER_GROUPBY 11
+#define FILTER_ORDERBY 12
+typedef int filter_t;
+
+#define OP_EQ 20
+#define OP_GT 21
+#define OP_GE 22
+#define OP_LT 23
+#define OP_LE 24
+#define OP_ASC 25
+#define OP_DESC 26
+typedef int filter_op_t;
+
 typedef struct {
 	char *colName;
 	char *value;
@@ -19,6 +33,10 @@ typedef struct {
 	action_t action;
 	char *table;
 	query_data_t *data;
+	filter_t filter;
+	char *filterField;
+	filter_op_t op;
+	char *filterValue;
 } query_t;
 
 /* Input:  La stringa contenente la query da eseguire, ad esempio "CREATE TABLE studenti (matricola, nome, cognome)"
